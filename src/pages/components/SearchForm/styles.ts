893b0eb912e0106339627bc24ff50be444a8b849
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-11-19 20:52:08 -0300

    raw: Creating the 'Transactions' page component, the application's home page, inside the 'pages' folder, along with the 'SearchForm' sub-component. Setting up the visual structure of the 'Transactions' page component by importing and applying the 'Header' and 'Summary' components to the page, and creating the structure and styles for the transactions table, listing all transactions made by the user, whether outgoing or incoming. Also creating the 'SearchForm' sub-component, representing a search form that allows users to search for specific transactions. Finally, importing the 'Transactions' component into the 'App' component

---
+++ src/pages/components/SearchForm/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+
+    padding: 1rem;
+    background: ${props => props.theme['gray-900']};
+
+    color: ${props => props.theme['gray-300']};
+
+    border: 0;
+    border-radius: 6px;
+  }
+
+  &::placeholder {
+    color: ${props => props.theme['gray-500']};
+  }
+
+  button {
+    padding: 1rem;
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    font-weight: bold;
+    color: ${props => props.theme['green-300']};
+
+    border: 1px solid ${props => props.theme['green-300']};
+    border-radius: 6px;
+
+    cursor: pointer;
+
+    &:hover {
+      background: ${props => props.theme['green-500']};
+
+      color: ${props => props.theme.white};
+
+      border: 1px solid ${props => props.theme['green-500']};
+
+      transition: all 0.2s;
+    }
+  }
+`
