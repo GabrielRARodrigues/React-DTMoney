5c4edbdafbbf6eb24dc7c4b6d33c4c80dce769df
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-12-02 21:34:50 -0300

    perf: Using React hooks 'useCallback' and 'useMemo' to memoize functions and values, improving the application's performance. Using the 'useCallback' hook on functions provided by the 'TransactionsContext' context to prevent these functions from being unnecessarily recreated when the context updates, ensuring that components utilizing these functions are not re-rendered unnecessarily. Using the 'useMemo' hook in the custom hook 'useSummary', located in the file 'useSummary.ts', to memoize the resulting value from the logic responsible for fetching the transaction summary

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 
 import { createContext } from 'use-context-selector'
 
@@ -35,7 +35,7 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
@@ -45,27 +45,30 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date()
-    })
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date()
+      })
 
-    setTransactions(prevState => {
-      return [response.data, ...prevState]
-    })
-  }
+      setTransactions(prevState => {
+        return [response.data, ...prevState]
+      })
+    },
+    []
+  )
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
